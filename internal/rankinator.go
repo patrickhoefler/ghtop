c1c912c00bd2bd7a5b14a8540a0807380e48fc43
Patrick Hoefler <patrick.hoefler@gmail.com>
2020-09-24 20:56:49 +0200

    Improve readability of the Rankinator (#2)

--- internal/rankinator.go
+++ internal/rankinator.go
@@ -23,36 +23,56 @@ type Rankinator struct {
 }
 
 // Get returns the calculated rank.
-// Please note that the values need to be supplied either in ascending or in descending order.
-func (r *Rankinator) Get(value int) (rank int, err error) {
-	if r.lastRank > 0 {
-		if value > r.lastValue {
-			if r.sortOrder == unknown {
-				r.sortOrder = ascending
-			} else if r.sortOrder == descending {
-				err = errors.New("sort order changed from descending to ascending")
-				return
-			}
-		} else if value < r.lastValue {
-			if r.sortOrder == unknown {
-				r.sortOrder = descending
-			} else if r.sortOrder == ascending {
-				err = errors.New("sort order changed from ascending to descending")
-				return
-			}
-		}
-	}
+// Note that the values need to be supplied either in ascending or in descending order.
+func (rankinator *Rankinator) Get(value int) (rank int, err error) {
+	rankinator.setSortOrder(value)
 
-	if value == r.lastValue {
-		r.sameValueCount++
-		rank = r.lastRank
+	err = rankinator.checkSortOrder(value)
+	if err != nil {
 		return
 	}
 
-	rank = r.lastRank + r.sameValueCount + 1
+	if value == rankinator.lastValue {
+		rankinator.sameValueCount++
+		rank = rankinator.lastRank
+		return
+	}
 
-	r.lastRank = rank
-	r.lastValue = value
-	r.sameValueCount = 0
+	rank = rankinator.lastRank + rankinator.sameValueCount + 1
+
+	rankinator.lastRank = rank
+	rankinator.lastValue = value
+	rankinator.sameValueCount = 0
+
 	return
+}
+
+func (rankinator *Rankinator) setSortOrder(value int) {
+	// Don't set the order on the first run
+	if rankinator.lastRank == 0 {
+		return
+	}
+
+	// Don't change the sort order once it has been set
+	if rankinator.sortOrder != unknown {
+		return
+	}
+
+	if value > rankinator.lastValue {
+		rankinator.sortOrder = ascending
+	} else if value < rankinator.lastValue {
+		rankinator.sortOrder = descending
+	}
+}
+
+func (rankinator *Rankinator) checkSortOrder(value int) error {
+	if value > rankinator.lastValue && rankinator.sortOrder == descending {
+		return errors.New("sort order changed from descending to ascending")
+	}
+
+	if value < rankinator.lastValue && rankinator.sortOrder == ascending {
+		return errors.New("sort order changed from ascending to descending")
+	}
+
+	return nil
 }
