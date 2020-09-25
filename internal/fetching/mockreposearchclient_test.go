f23e53107a15ed673a4944f9ffade8b5552ce64b
Patrick Hoefler <patrick.hoefler@gmail.com>
2020-09-25 20:37:12 +0200

    More tests

---
+++ internal/fetching/mockreposearchclient_test.go
@@ -0,0 +1,65 @@
+package fetching
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/google/go-github/v32/github"
+)
+
+func TestNewMockRepoSearchClient(t *testing.T) {
+	tests := []struct {
+		name string
+		want Fetcher
+	}{
+		{
+			name: "default",
+			want: newRepoSearchClient(new(mockGitHubSearchClient)),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewMockRepoSearchClient(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewMockRepoSearchClient() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_mockGitHubSearchClient_Repositories(t *testing.T) {
+	type args struct {
+		ctx   context.Context
+		query string
+		opts  *github.SearchOptions
+	}
+	tests := []struct {
+		name             string
+		mghcs            *mockGitHubSearchClient
+		args             args
+		wantSearchResult *github.RepositoriesSearchResult
+		wantResp         *github.Response
+		wantErr          bool
+	}{
+		{
+			name: "default",
+			args: args{
+				opts: &github.SearchOptions{
+					ListOptions: github.ListOptions{
+						PerPage: 100,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mghcs := &mockGitHubSearchClient{}
+			_, _, err := mghcs.Repositories(tt.args.ctx, tt.args.query, tt.args.opts)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("mockGitHubSearchClient.Repositories() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
