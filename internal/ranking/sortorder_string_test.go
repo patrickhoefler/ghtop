96d417656635e273a1022e3534322088d8b4d3ea
Patrick Hoefler <patrick.hoefler@gmail.com>
2020-09-25 21:17:52 +0200

    One more

--- internal/ranking/sortorder_string_test.go
+++ internal/ranking/sortorder_string_test.go
@@ -23,6 +23,16 @@ func Test_sortOrder_String(t *testing.T) {
 			i:    2,
 			want: "descending",
 		},
+		{
+			name: "negative",
+			i:    -1,
+			want: "sortOrder(-1)",
+		},
+		{
+			name: "undefined",
+			i:    42,
+			want: "sortOrder(42)",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
