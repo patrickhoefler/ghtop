df88fe9981003abfdba5fc43f1a82b9343f29262
Patrick Hoefler <patrick.hoefler@gmail.com>
2022-12-22 17:00:31 +0100

    Bump go-github to v48

--- internal/fetching/fetcher.go
+++ internal/fetching/fetcher.go
@@ -1,6 +1,6 @@
 package fetching
 
-import "github.com/google/go-github/v39/github"
+import "github.com/google/go-github/v48/github"
 
 // Fetcher defines the interface for repo fetchers
 type Fetcher interface {
--- internal/fetching/githubreposearchclient_test.go
+++ internal/fetching/githubreposearchclient_test.go
@@ -4,7 +4,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/google/go-github/v39/github"
+	"github.com/google/go-github/v48/github"
 )
 
 func TestNewGitHubRepoSearchClient(t *testing.T) {
